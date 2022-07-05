9eb5b51636f63e53a4869a399d52654ba37ab026
Michal 'Sand3r-' Gallus <michal.gallus@gmail.com>
2022-07-05 21:19:02 +0200

    Implement new Mat4 Functions

--- src/math/mat.h
+++ src/math/mat.h
@@ -6,6 +6,7 @@ union m4
 {
     v4 rows[4];
     f32 elems[4][4];
+    f32 m[4][4];
 };
 
 typedef union m3 m3;
@@ -13,10 +14,14 @@ union m3
 {
     v3 rows[3];
     f32 elems[3][3];
+    f32 m[3][3];
 };
 
 // Column-Major is assumed
 m4 M4Init(f32 diagonal);
 m4 M4Multiply(m4 a, m4 b);
+v4 M4MultiplyV4(m4 a, v4 b);
 m4 Perspective(f32 fov_deg, f32 aspect, f32 near_z, f32 far_z);
 m4 LookAt(v3 eye, v3 target, v3 up);
+m4 Inverse(m4 m);
+f32 Determinant(m4 m);
