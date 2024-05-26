a1fe47f55d487d75addf721e6679b0fd0f5624f6
Michal 'Sand3r-' Gallus <michal.gallus@gmail.com>
2024-05-26 11:22:51 +0200

    Implement basic script editor functionality

--- src/ui/scripteditor.cpp
+++ src/ui/scripteditor.cpp
@@ -1,7 +1,10 @@
 #include "scripteditor.h"
 #include "TextEditor.h"
 extern "C" {
+    #include "log/log.h"
+    #include "script/scripting.h"
     #include "memory/linearallocator.h"
+    #include "file.h"
 }
 #include <fstream>
 #include <streambuf>
@@ -33,56 +36,128 @@ void InitializeScriptEditor(void)
     }
 }
 
-void UpdateScriptEditor(void)
+static void NewFile()
 {
-    ImGui::Begin("Text Editor Demo", nullptr, ImGuiWindowFlags_HorizontalScrollbar | ImGuiWindowFlags_MenuBar);
-    ImGui::SetWindowSize(ImVec2(800, 600), ImGuiCond_FirstUseEver);
-     if (ImGui::BeginMenuBar())
-     {
-        if (ImGui::BeginMenu("File"))
-        {
-            if (ImGui::MenuItem("Save"))
-            {
-                auto text = C.editor->GetText();
-                // TODO: Save file contents
-            }
-            ImGui::EndMenu();
-        }
-        if (ImGui::BeginMenu("Edit"))
-        {
-            if (ImGui::MenuItem("Undo", "Ctrl+Z", nullptr, C.editor->CanUndo()))
-                C.editor->Undo();
-            if (ImGui::MenuItem("Redo", "Ctrl-Y", nullptr, C.editor->CanRedo()))
-                C.editor->Redo();
+    C.file_name = "Untitled";
+    C.text->clear();
+    C.editor->SetText(*C.text);
+}
 
-            ImGui::Separator();
+static void SaveFile(const char* filename = "Untitled")
+{
+    File file = FileOpen(filename, "w");
+    FileWrite(&file, (void*)C.editor->GetText().c_str(), sizeof(char), C.editor->GetText().size());
+    FileClose(&file);
+}
 
-            if (ImGui::MenuItem("Copy", "Ctrl-C", nullptr, C.editor->AnyCursorHasSelection()))
-                C.editor->Copy();
-            if (ImGui::MenuItem("Cut", "Ctrl-X", nullptr, C.editor->AnyCursorHasSelection()))
-                C.editor->Cut();
-            if (ImGui::MenuItem("Paste", "Ctrl-V", nullptr, ImGui::GetClipboardText() != nullptr))
-                C.editor->Paste();
+static void OpenFile(const char* file_name)
+{
+    C.file_name = file_name;
+    File file = FileOpen(file_name, "r");
+    size_t size = FileRead(&file, (void*)C.text->data(), sizeof(char), C.text->size());
+    FileClose(&file);
+    C.text->resize(size);
+    C.editor->SetText(*C.text);
+}
 
-            ImGui::Separator();
+static void Execute()
+{
+    ExecuteScript(C.editor->GetText().c_str());
+}
 
-            if (ImGui::MenuItem("Select all", "Ctrl+A", nullptr))
-                C.editor->SelectAll();
+static void HandleKeyboardInputs()
+{
+    ImGuiIO& io = ImGui::GetIO();
+    auto alt = io.KeyAlt;
+    auto ctrl = io.KeyCtrl;
+    auto shift = io.KeyShift;
+    auto super = io.KeySuper;
 
-            ImGui::EndMenu();
-        }
-        ImGui::EndMenuBar();
-     }
+    auto isAltOnly = alt && !ctrl && !shift && !super;
+    auto isCtrlOnly = ctrl && !alt && !shift && !super;
+    auto isShiftOnly = shift && !alt && !ctrl && !super;
 
-    // TODO: Move this to the bottom as it's distracting to have it on top.
-    // Currently replacing the calls to Text and pEditor->Render
-    // results in screen flickering due to some bug in TextEditor.
+    io.WantCaptureKeyboard = true;
+    io.WantTextInput = true;
+
+    if (isCtrlOnly && ImGui::IsKeyPressed(ImGuiKey_S))
+        SaveFile(C.file_name);
+    else if (isCtrlOnly && ImGui::IsKeyPressed(ImGuiKey_O))
+        OpenFile("Untitled");
+    else if (isCtrlOnly && ImGui::IsKeyPressed(ImGuiKey_N))
+        NewFile();
+    else if (ImGui::IsKeyPressed(ImGuiKey_F5))
+        Execute();
+}
+
+static void DrawStatusBar()
+{
+    constexpr float textWidth = 300;
     int line, column;
     C.editor->GetCursorPosition(line, column);
+    ImGui::SameLine(ImGui::GetContentRegionAvail().x - textWidth);
+    ImGui::AlignTextToFramePadding();
     ImGui::Text("Ln %3d, Col %-2d | %s | %s%s", line + 1, column + 1,
         C.editor->GetLanguageDefinitionName(), C.file_name,
         C.editor->CanUndo() ? "*" : " ");
+}
 
-    C.editor->Render("TextEditor");
+void UpdateScriptEditor(void)
+{
+    if (ImGui::Begin("Script Editor", nullptr, ImGuiWindowFlags_MenuBar))
+    {
+        ImGui::SetWindowSize(ImVec2(800, 600), ImGuiCond_FirstUseEver);
+        if (ImGui::BeginMenuBar())
+        {
+            if (ImGui::BeginMenu("File"))
+            {
+                if (ImGui::MenuItem("New", "Ctrl+N"))
+                    NewFile();
+                if (ImGui::MenuItem("Save", "Ctrl+S"))
+                    SaveFile();
+                ImGui::MenuItem("Save As", "Ctrl+Shift+S", nullptr, false);
+                if (ImGui::MenuItem("Open", "Ctrl+O"))
+                    OpenFile("Untitled");
+                ImGui::EndMenu();
+            }
+            if (ImGui::BeginMenu("Edit"))
+            {
+                if (ImGui::MenuItem("Undo", "Ctrl+Z", nullptr, C.editor->CanUndo()))
+                    C.editor->Undo();
+                if (ImGui::MenuItem("Redo", "Ctrl-Y", nullptr, C.editor->CanRedo()))
+                    C.editor->Redo();
+
+                ImGui::Separator();
+
+                if (ImGui::MenuItem("Copy", "Ctrl-C", nullptr, C.editor->AnyCursorHasSelection()))
+                    C.editor->Copy();
+                if (ImGui::MenuItem("Cut", "Ctrl-X", nullptr, C.editor->AnyCursorHasSelection()))
+                    C.editor->Cut();
+                if (ImGui::MenuItem("Paste", "Ctrl-V", nullptr, ImGui::GetClipboardText() != nullptr))
+                    C.editor->Paste();
+
+                ImGui::Separator();
+
+                if (ImGui::MenuItem("Select all", "Ctrl+A", nullptr))
+                    C.editor->SelectAll();
+
+                ImGui::EndMenu();
+            }
+            ImGui::EndMenuBar();
+        }
+        try
+        {
+            C.editor->Render("TextEditor");
+        }
+        catch (...) // Any exception
+        {
+            const char* file = "recovered_file.txt";
+            L_FATAL("Script Editor crashed. Saving backup to %s", file);
+            SaveFile(file);
+            exit(EXIT_FAILURE);
+        }
+        DrawStatusBar();
+        HandleKeyboardInputs();
+    }
     ImGui::End();
 }
